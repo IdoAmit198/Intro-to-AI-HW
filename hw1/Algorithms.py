9786fe5bfbdad81f4eb003c8fb1ed4278c0d30d7
IdoAmit198 <dodoamit198@gmail.com>
2024-07-09 08:48:11 +0300

    Pushing current 09/07

--- hw1/Algorithms.py
+++ hw1/Algorithms.py
@@ -6,7 +6,7 @@ from typing import List, Tuple
 import heapdict
 
 class Node:
-    def __init__(self,parent,action_creatiation, cell , cost, heuristic, total_cost):
+    def __init__(self,parent,action_creatiation, cell , cost, heuristic, total_cost, f_value = 0):
         self.parent = parent
         self.action_creatiation = action_creatiation
         self.cell = cell
@@ -15,8 +15,9 @@ class Node:
         self.heuristic = heuristic
         # self.total_cost = np.inf
         self.total_cost = total_cost
+        self.f_value = self.heuristic if f_value is None else f_value
 
-    def expend(self,env, Agent_search_type:str = None):
+    def expend(self,env, Agent_search_type:str = None, h_weight=0):
         if self.total_cost == np.inf:
             return []
         children = []
@@ -25,7 +26,15 @@ class Node:
             new_state, cost, terminated = env.step(action)
             # if Agent_search_type == str(DFSGAgent) and cost != np.inf:
             #     cost=1
-            child = Node(self,action,new_state,cost,None,self.total_cost+cost)
+            new_state_huristic = HCampus_huristic(new_state, env)
+            f_value = (1-h_weight)*(self.total_cost + cost) + h_weight*new_state_huristic
+            child = Node(parent = self,
+                         action_creatiation = action,
+                         cell = new_state,
+                         cost = cost,
+                         heuristic = HCampus_huristic(new_state, env),
+                         total_cost = self.total_cost+cost,
+                         f_value = f_value)
             children.append(child)
             env.set_state(node_state)
             # env.step((action+2)%4)
@@ -88,7 +97,7 @@ class UCSAgent(Agent):
         self.start_node = Node(None,None,0,1,None,0)
         # self.start_node = Node(0,0,0,1,0,0)
         self.expanded = 0
-        # self.open.append(self.start_node)
+        self.expanded_list = []
         self.open[self.start_node.cell] = (self.start_node.cost, self.start_node.cell, self.start_node) # Each element is a tuple of (cost, state)
         super().__init__()
 
@@ -104,43 +113,79 @@ class UCSAgent(Agent):
                 else:
                     print(f"node.cell is None.")
                 if self.env.is_final_state(node.cell):
+                    print(self.expanded_list)
                     actions_path, total_cost = self.solution(node)
                     return actions_path, total_cost, self.expanded
                 children = node.expend(self.env, str(type(self)))
                 self.expanded += 1
+                self.expanded_list.append(node.cell)
                 if len(children)>0:
                     for child in children:
                         if self.closed.get(child.cell) is None and self.open.get(child.cell) is None: # self.open keep Node objects, might be wrong to check it, and better to check for integers of cells.
-                            self.open[child.cell] = (child.cost, child.cell, child)
-                        elif self.open.get(child.cell) and self.open.get(child.cell)[0]>child.cost:
-                            self.open[child.cell] = (child.cost, child.cell, child)
+                            self.open[child.cell] = (child.total_cost, child.cell, child)
+                        elif self.open.get(child.cell) and self.open.get(child.cell)[0]>child.total_cost:
+                            self.open[child.cell] = (child.total_cost, child.cell, child)
             except KeyError:
                 print("No solution found.")
                 return None
 
+def HCampus_huristic(state:int, env:CampusEnv) -> float:
+    row, col = env.to_row_col(state)
+    goals_list = env.get_goal_states()
+    goals_rows_cols = [env.to_row_col(goal) for goal in goals_list]
+    # Compute manhatan distance from the state to each of the goals
+    manhatan_dist_list = [abs(goal_row-row)+abs(goal_col-col) for goal_row, goal_col in goals_rows_cols]
+    min_manhatan = min(manhatan_dist_list)
+    return min(min_manhatan, 100)
 
 class WeightedAStarAgent(Agent):
-    
     def __init__(self):
         self.closed={}
         self.open = heapdict.heapdict()
-        self.start_node = Node(None,None,0,1,None,0)
-        # self.start_node = Node(0,0,0,1,0,0)
         self.expanded = 0
-        # self.open.append(self.start_node)
-        self.open[self.start_node.cell] = (self.start_node.cost, self.start_node.cell, self.start_node) # Each element is a tuple of (cost, state)
+        self.expanded_list = []
         super().__init__()
 
+    
     def search(self, env: CampusEnv, h_weight) -> Tuple[List[int], float, int]:
-        raise NotImplementedError   
+        self.env = env
+        self.env.reset()
+        self.start_node = Node(None,None,0,1,HCampus_huristic(state=0, env=env),0, HCampus_huristic(state=0, env=env))   
+        self.open[self.start_node.cell] = (self.start_node.f_value, self.start_node.cell, self.start_node) # Each element is a tuple of (cost, state)
+        while True:
+            try:
+                node = self.open.popitem()[1][2]
+                self.closed[node.cell]=node.total_cost
+                if node.cell is not None:
+                    self.env.set_state(node.cell)
+                else:
+                    print(f"node.cell is None.")
+                if self.env.is_final_state(node.cell):
+                    print(self.expanded_list)
+                    actions_path, total_cost = self.solution(node)
+                    return actions_path, total_cost, self.expanded
+                children = node.expend(self.env, str(type(self)), h_weight=h_weight)
+                self.expanded += 1
+                self.expanded_list.append(node.cell)
+                if len(children)>0:
+                    for child in children:
+                        if self.closed.get(child.cell) is None and self.open.get(child.cell) is None:
+                            self.open[child.cell] = (child.f_value, child.cell, child)
+                        elif self.open.get(child.cell) and self.open.get(child.cell)[2].total_cost>child.total_cost:
+                            self.open[child.cell] = (child.f_value, child.cell, child)
+                        elif self.closed.get(child.cell) and self.closed.get(child.cell)>child.total_cost:
+                            self.closed.pop(child.cell)
+                            self.open[child.cell] = (child.f_value, child.cell, child)
+            except IndexError:
+                print("Heap was empty. No solution found.")
+                return None
 
 
-
-class AStarAgent(Agent):
+class AStarAgent():
     
     def __init__(self):
-        raise NotImplementedError
+        self.weighted_A_star = WeightedAStarAgent()
 
     def search(self, env: CampusEnv) -> Tuple[List[int], float, int]:
-        raise NotImplementedError 
+        return self.weighted_A_star.search(env=env, h_weight=0.5)
 
--- hw1/run.py
+++ hw1/run.py
@@ -72,14 +72,111 @@ if __name__ == "__main__":
 
   # UCS_agent = UCSAgent()
   # actions, total_cost, expanded = UCS_agent.search(env)
-  WAstar_agent = WeightedAStarAgent()
-actions, total_cost, expanded = WAstar_agent.search(env, h_weight=0.7)
-  print(f"Total_cost: {total_cost}")
-  print(f"Expanded: {expanded}")
-  print(f"Actions: {actions}")
-
+  # WAstar_agent = WeightedAStarAgent()
+  # actions, total_cost, expanded = WAstar_agent.search(env, h_weight=0.7)
+  # print(f"Total_cost: {total_cost}")
+  # print(f"Expanded: {expanded}")
+  # print(f"Actions: {actions}")
+  # Astar_agent = AStarAgent()
+  # actions, total_cost, expanded = Astar_agent.search(env)
+  # print(f"Total_cost: {total_cost}")
+  # print(f"Expanded: {expanded}")
+  # print(f"Actions: {actions}")
   # UCS_agent = UCSAgent()
   # actions, total_cost, expanded = UCS_agent.search(env_4)
   # print(f"Total_cost: {total_cost}")
   # print(f"Expanded: {expanded}")
   # print(f"Actions: {actions}")
+
+  import csv
+
+test_maps = {
+    "map12x12": ['SFAFTFFTHHHF',
+                 'AFLTFFFFTALF',
+                 'LHHLLHHLFTHP',
+                 'HALTHAHHAPHF',
+                 'FFFTFHFFAHFL',
+                 'LLTHFFFAHFAT',
+                 'HAAFFALHTATF',
+                 'LLLFHFFHTLFH',
+                 'FATAFHTTFFAF',
+                 'HHFLHALLFTLF',
+                 'FFAFFTTAFAAL',
+                 'TAAFFFHAFHFG'],
+    "map15x15": ['SFTTFFHHHHLFATF',
+                 'ALHTLHFTLLFTHHF',
+                 'FTTFHHHAHHFAHTF',
+                 'LFHTFTALTAAFLLH',
+                 'FTFFAFLFFLFHTFF',
+                 'LTAFTHFLHTHHLLA',
+                 'TFFFAHHFFAHHHFF',
+                 'TTFFLFHAHFFTLFP',
+                 'TFHLHTFFHAAHFHF',
+                 'HHAATLHFFLFFHLH',
+                 'FLFHHAALLHLHHAT',
+                 'TLHFFLTHFTTFTTF',
+                 'AFLTPAFTLHFHFFF',
+                 'FFTFHFLTAFLHTLA',
+                 'HTFATLTFHLFHFAG'],
+    "map20x20" : ['SFFLHFHTALHLFATAHTHT',
+                  'HFTTLLAHFTAFAAHHTLFH',
+                  'HHTFFFHAFFFFAFFTHHHT',
+                  'TTAFHTFHTHHLAHHAALLF',
+                  'HLALHFFTHAHHAFFLFHTF',
+                  'AFTAFTFLFTTTFTLLTHPF',
+                  'LFHFFAAHFLHAHHFHFALA',
+                  'AFTFFLTFLFTAFFLTFAHH',
+                  'HTTLFTHLTFAFFLAFHFTF',
+                  'LLALFHFAHFAALHFTFHTF',
+                  'LFFFAAFLFFFFHFLFFAFH',
+                  'THHTTFAFLATFATFTHLLL',
+                  'HHHAFFFATLLALFAHTHLL',
+                  'HLFFFFHFFLAAFTFFPAFH',
+                  'HTLFTHFFLTHLHHLHFTFH',
+                  'AFTTLHLFFLHTFFAHLAFT',
+                  'HAATLHFFFHHHHAFFFHLH',
+                  'FHFLLLFHLFFLFTFFHAFL',
+                  'LHTFLTLTFATFAFAFHAAF',
+                  'FTFFFFFLFTHFTFLTLHFG'],
+}
+
+test_envs = {}
+for map_name, map_inst in test_maps.items():
+    test_envs[map_name] = CampusEnv(map_inst)
+
+
+DFSG_agent = DFSGAgent()
+UCS_agent = UCSAgent()
+WAStar_agent = WeightedAStarAgent()
+AStar_agent = AStarAgent()
+
+weights = [0.3, 0.7, 0.9]
+
+agents_search_function = [
+    DFSG_agent.search,
+    UCS_agent.search,
+    AStar_agent.search,
+]
+
+header = [
+    'map',
+    'DFS-G cost', 'DFS-G num of expanded nodes',
+    'UCS cost', 'UCS  num of expanded nodes',
+    'A* cost', 'A*  num of expanded nodes',
+    'W-A* (0.3) cost', 'W-A* (0.3) num of expanded nodes',
+    'W-A* (0.7) cost', 'W-A* (0.7) num of expanded nodes',
+    'W-A* (0.9) cost', 'W-A* (0.9) num of expanded nodes',
+]
+
+with open("results.csv", 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    for env_name, env in test_envs.items():
+        data = [env_name]
+        for agent in agents_search_function:
+            _, total_cost, expanded = agent(env)
+            data += [total_cost, expanded]
+        for w in weights:
+            _, total_cost, expanded = WAStar_agent.search(env, w)
+            data += [total_cost, expanded]
+        writer.writerow(data)
