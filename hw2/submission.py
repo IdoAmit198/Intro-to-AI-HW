bf4543ee1360cf07326271d734770887fc764ea3
IdoAmit198 <dodoamit198@gmail.com>
2024-08-01 19:14:48 +0300

    Ready to sbumit

--- hw2/submission.py
+++ hw2/submission.py
@@ -6,13 +6,12 @@ import random
 import math
 import time
 
+Margin = 0.91
 
 # TODO: section a : 3
 
 def is_holding_package(robot: Robot):
-    if robot.package is None:
-        return False
-    return True
+    return robot.package is not None
 
 def batteryR(env: WarehouseEnv, robot_id: int, robot_dist_from_pack_credit: int):
     my_robot = env.get_robot(robot_id)
@@ -24,50 +23,6 @@ def batteryR(env: WarehouseEnv, robot_id: int, robot_dist_from_pack_credit: int)
     dist_to_closest_charge_station =  min([manhattan_distance(my_robot.position, chrage_station.position) \
                                            for chrage_station in env.charge_stations])
     return dist_to_closest_charge_station
-
-
-##################################################################
-def manhattan_dist(pos1, pos2):
-    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
-
-def has_package(robot):
-    return robot.package is not None
-
-def dist(env, robot_id):
-    robot = env.get_robot(robot_id)
-    other_robot = env.get_robot((robot_id + 1) % 2)
-    if has_package(robot):
-        if robot.position == robot.package.destination:
-            return -(board_size ** 3)
-        return manhattan_dist(robot.position, robot.package.destination)
-    elif has_package(other_robot):
-        assert len(env.packages) == 3
-        free_package = env.packages[0]
-        return manhattan_dist(robot.position, free_package.position) + \
-            manhattan_dist(free_package.position, free_package.destination)
-    else:
-        packages = env.packages
-        dist_to_package = dict()
-        min_dist = 15
-        closest_package = None
-        for package in packages:
-            my_dist = manhattan_dist(robot.position, package.position)
-            if my_dist < min_dist:
-                min_dist = my_dist
-                closest_package = package
-            dist_to_package[package] = (my_dist, manhattan_dist(other_robot.position, package.position))
-        if dist_to_package[closest_package][0] < dist_to_package[closest_package][1]:
-            return dist_to_package[closest_package][0] + manhattan_dist(closest_package.position,
-                                                                        closest_package.destination)
-        other_package = None
-        for package in packages:
-            if package != closest_package:
-                other_package = package
-                break
-        return dist_to_package[other_package][0] + manhattan_dist(other_package.position, other_package.destination)
-
-
-########################################################################
 
 
 def stepsR(env: WarehouseEnv, robot_id: int, T: int):
@@ -113,16 +68,12 @@ def stepsR(env: WarehouseEnv, robot_id: int, T: int):
 
 def smart_heuristic(env: WarehouseEnv, robot_id: int):
     credit_weight = 5
-    # T = 7 * board_size**2
     T = 4
     my_robot = env.get_robot(robot_id)
     opponet_robot = env.get_robot((robot_id + 1) % 2)
     my_robot_dist_from_package_credit = stepsR(env=env, robot_id=robot_id, T=T)
-    # d = dist(env, robot_id)
     heuristic =  credit_weight * (my_robot.credit - opponet_robot.credit) \
-                - batteryR(env, robot_id, my_robot_dist_from_package_credit) \
-                - my_robot_dist_from_package_credit 
-                # - batteryR(env, robot_id, d) -d
+                - batteryR(env, robot_id, my_robot_dist_from_package_credit) - my_robot_dist_from_package_credit 
     return heuristic
 
 
@@ -134,7 +85,7 @@ class AgentGreedyImproved(AgentGreedy):
 
 class AgentMinimax(Agent):
     def run_step(self, env: WarehouseEnv, agent_id: int, time_limit):
-        self.time_limit= time.time()+0.9*time_limit
+        self.time_limit= time.time() + Margin*time_limit
         return self.minimax(env,agent_id)
     
     #main function
@@ -153,9 +104,6 @@ class AgentMinimax(Agent):
     def RB_minimax(self, env: WarehouseEnv, agent_id: int, depth):
         operators, children = self.successors(env, agent_id)
         childs_heuristic = [self.min_func(child, agent_id + 1, depth - 1) for child in children]
-        # print(f"for depth {depth}:\n")
-        # for op, res in zip(operators, childs_heuristic):
-        #     print(f"op: {op}, got heuristic: {res}")
         max_heuristic_child, max_heuristic_idx = max(childs_heuristic), int(np.argmax(childs_heuristic))
         return operators[max_heuristic_idx], max_heuristic_child
     
@@ -180,12 +128,12 @@ class AgentMinimax(Agent):
 
 class AgentAlphaBeta(Agent):
     # section c : 1 - done
-    def run_step(self, env: WarehouseEnv, agent_id, time_limit):
+    def run_step(self, env: WarehouseEnv, agent_id: int, time_limit):
         # TODO: Refactor the time_limit computation.
-        self.time_limit = time.time() + 0.9 * time_limit
-        return self.id_alpha_beta(env, agent_id)
+        self.time_limit = time.time() + Margin * time_limit
+        return self.alpha_beta(env, agent_id)
 
-    def max_func(self, env: WarehouseEnv, agent_id: int, depth, alpha, beta):
+    def max_func(self, env: WarehouseEnv, agent_id: int, depth: int, alpha, beta):
         agent_id = agent_id % 2
         if time.time() > self.time_limit or depth == 0:
             return self.heuristic(env, agent_id)
@@ -200,7 +148,7 @@ class AgentAlphaBeta(Agent):
             alpha = max(curr_max, alpha)
         return curr_max
     
-    def min_func(self, env: WarehouseEnv, agent_id: int, depth, alpha, beta):
+    def min_func(self, env: WarehouseEnv, agent_id: int, depth: int, alpha, beta):
         agent_id = agent_id % 2
         if time.time() > self.time_limit or depth == 0:
             return smart_heuristic(env, agent_id)
@@ -216,33 +164,27 @@ class AgentAlphaBeta(Agent):
         return curr_min
 
 
-    def id_alpha_beta(self, env: WarehouseEnv, agent_id: int):
+    def alpha_beta(self, env: WarehouseEnv, agent_id: int):
         D = 2
         result_operator, result_heuristic = None, -math.inf
         while time.time() < self.time_limit:
-            curr_operator, curr_heuristic = self.alpha_beta(env, agent_id, D, -math.inf, math.inf)
+            curr_operator, curr_heuristic = self.RB_alpha_beta(env, agent_id, D, -math.inf, math.inf)
             if curr_heuristic > result_heuristic:
                 result_operator = curr_operator
                 result_heuristic = curr_heuristic
             D = D + 2
         return result_operator
-        # while time.time() < self.time_limit:
-        #     res = self.alpha_beta(env, agent_id, D, -math.inf, math.inf)
-        #     D = D + 2
-        # return res
 
-    def alpha_beta(self, env: WarehouseEnv, agent_id: int, depth, alpha, beta):
+    def RB_alpha_beta(self, env: WarehouseEnv, agent_id: int, depth: int, alpha, beta):
         operators, children = self.successors(env, agent_id)
         childs_heuristic = [self.min_func(child, agent_id + 1, depth - 1, alpha, beta) for child in children]
-        # max_children_idx = int(np.argmax(children_results))
-        # index_selected = children_results.index(max_children)
         max_heuristic_child, max_heuristic_idx = max(childs_heuristic), int(np.argmax(childs_heuristic))
         return operators[max_heuristic_idx], max_heuristic_child
 
 
 class AgentExpectimax(Agent):
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        self.time_limit= time.time()+0.9*time_limit
+        self.time_limit= time.time() + Margin*time_limit
         return self.expectimax(env,agent_id)
 
     #main function
@@ -252,9 +194,6 @@ class AgentExpectimax(Agent):
         chosen_operator, result_max_val = None, -math.inf
         while time.time() < self.time_limit:
             operators, children = self.successors(env, agent_id)
-            # max_val = None
-            # chosen_op = -1
-            # for child in children:
             children_vals = [self.RB_expectimax(child,agent_id + 1,D-1) for child in children]
             max_val_child, max_val_idx = max(children_vals), int(np.argmax(children_vals))
             if max_val_child > result_max_val:
@@ -264,7 +203,7 @@ class AgentExpectimax(Agent):
         return chosen_operator
 
     #inner expectimax
-    def RB_expectimax(self, env: WarehouseEnv, agent_id: int, depth):
+    def RB_expectimax(self, env: WarehouseEnv, agent_id: int, depth: int):
         agent_id = agent_id % 2
         if (time.time() > self.time_limit) or depth==0 or env.done(): 
             return smart_heuristic(env, agent_id)
@@ -273,7 +212,6 @@ class AgentExpectimax(Agent):
         else: 
             return self.probablistic_func(env, agent_id, depth)
     
-
     #if it is the agent turn
     def max_func(self, env: WarehouseEnv, agent_id: int, depth):
         agent_id = agent_id % 2
@@ -284,7 +222,7 @@ class AgentExpectimax(Agent):
         return max(max(children_vector),cur_max)
         
     #if it is the other agent turn and min part
-    def min_func(self, env: WarehouseEnv, agent_id: int, depth):
+    def min_func(self, env: WarehouseEnv, agent_id: int, depth: int):
         agent_id = agent_id % 2
         if (time.time() > self.time_limit) or depth==0 or env.done(): return smart_heuristic(env,agent_id)
         _, children = self.successors(env, agent_id)
@@ -293,7 +231,7 @@ class AgentExpectimax(Agent):
         return min(min(children_vector),cur_min)
     
     #if we need to do expectimax on the results of the other agent
-    def probablistic_func(self, env: WarehouseEnv, agent_id: int, depth):
+    def probablistic_func(self, env: WarehouseEnv, agent_id: int, depth: int):
         agent_id = agent_id % 2
         if (time.time() > self.time_limit) or depth==0 or env.done(): return smart_heuristic(env,agent_id)
         operators, children = self.successors(env, agent_id)
@@ -302,8 +240,7 @@ class AgentExpectimax(Agent):
             if operator == 'move east' or operator == 'pick up':
                 operators_prob.append(2)
             else:
-                operators_prob.append(1)
-        
+                operators_prob.append(1) 
         denominator = sum(operators_prob)
         operators_prob = [op / denominator for op in operators_prob]
         heuristics = [self.min_func(child, agent_id + 1, depth - 1) for child in children]
